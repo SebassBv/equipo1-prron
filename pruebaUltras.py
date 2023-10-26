c988ce02583680a2b338104f5a8691946e7d2d7c
SebassBv <sebasstian.rojass@outlook.com>
2023-10-25 20:27:03 -0600

    Ahora si jala

--- pruebaUltras.py
+++ pruebaUltras.py
@@ -1,19 +1,20 @@
 from gpiozero import DistanceSensor
 import subprocess
 
-topic = "testTopic"
+topic = "gauge1"
 message = None
 # Definir los pines GPIO para los LEDs y el sensor ultrasónico
 sensor = DistanceSensor(echo=15, trigger=14)
+
+message = str(sensor.distance)
 
 # Construye el comando mosquitto_pub
 command = ["mosquitto_pub", "-t", topic, "-m", message]
 
 # Ejecuta el comando
-try:
-    while True:
-        message = str(sensor.distance)
+while True:
+    try:
         subprocess.run(command, check=True)
         print(message)
-except subprocess.CalledProcessError as e:
-    print(f"Error al enviar el mensaje: {e}")
+    except subprocess.CalledProcessError as e:
+        print(f"Error al enviar el mensaje: {e}")

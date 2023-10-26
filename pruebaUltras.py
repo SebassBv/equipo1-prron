a94a676d2177f2d394efd8c9ec8c16d780f93e5c
SebassBv <sebasstian.rojass@outlook.com>
2023-10-25 20:34:49 -0600

    código con time 2 seg

--- pruebaUltras.py
+++ pruebaUltras.py
@@ -1,5 +1,6 @@
 from gpiozero import DistanceSensor
 import subprocess
+import time
 
 topic = "gauge1"
 message = None
@@ -8,9 +9,10 @@ sensor = DistanceSensor(echo=15, trigger=14)
 # Ejecuta el comando
 while True:
     try:
-        message = str(sensor.distance)
+        message = str((sensor.distance)*100)
         # Construye el comando mosquitto_pub
         command = ["mosquitto_pub", "-t", topic, "-m", message]
         subprocess.run(command, check=True)
+        time.sleep(2)
     except subprocess.CalledProcessError as e:
         print(f"Error al enviar el mensaje: {e}")

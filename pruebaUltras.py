724c5285b8aeedec6e2e38846ba0bf7b92e85cdb
SebassBv <sebasstian.rojass@outlook.com>
2023-10-25 20:29:28 -0600

    Jalaa porque si

--- pruebaUltras.py
+++ pruebaUltras.py
@@ -5,16 +5,12 @@ topic = "gauge1"
 message = None
 # Definir los pines GPIO para los LEDs y el sensor ultrasónico
 sensor = DistanceSensor(echo=15, trigger=14)
-
-message = str(sensor.distance)
-
-# Construye el comando mosquitto_pub
-command = ["mosquitto_pub", "-t", topic, "-m", message]
-
 # Ejecuta el comando
 while True:
     try:
+        message = str(sensor.distance)
+        # Construye el comando mosquitto_pub
+        command = ["mosquitto_pub", "-t", topic, "-m", message]
         subprocess.run(command, check=True)
-        print(message)
     except subprocess.CalledProcessError as e:
         print(f"Error al enviar el mensaje: {e}")

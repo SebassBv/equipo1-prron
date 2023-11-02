a368fc9c4abdd6b08346c9718999a775ad4ea234
Equipo1Raspberry <andresdavid_2000@hotmail.com>
2023-11-01 20:04:32 -0600

    Avance Leds

--- post.py
+++ post.py
@@ -4,18 +4,13 @@ app = Flask(__name__)
 
 @app.route("/method_post", methods=["GET", "POST"])
 def method_post():
-    return render_template("method_post.html")
+    return render_template("post_led.html")
 
 @app.route('/method_post_act', methods=["GET",  "POST"])
 def method_post_act():
     if request.method == "POST":
-        id = request.form["id"]
-        password = request.form["password"]
-        return render_template("method_post.html", id=id, password=password)
-    
-@app.route('/easterEgg')
-def easterEgg():
-    return 'Entraste al lado oscuro de la RPI 0.o'
+        NumLed = request.form["NumLed"]
+        return render_template("post_led.html", NumLed=NumLed)
     
 if __name__ == '__main__':
     app.run(debug=True, port=88, host='0.0.0.0')    

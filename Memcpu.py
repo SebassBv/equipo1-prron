8b7395e3bc8e4b6de8ab0ccffe57db36dd2d2075
SebassBv <sebasstian.rojass@outlook.com>
2023-11-01 20:00:00 -0600

    Ejercicio Memoria CPU

---
+++ Memcpu.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from gpiozero import LED
+import psutil
+
+app = Flask(__name__)
+
+@app.route('/CaracteristicasDelServidor')
+def getCaracteristicas(): 
+    memory = psutil.virtual_memory().used * 100 / psutil.virtual_memory().total
+    cpu = psutil.cpu_percent(interval=1)
+    return ("Memoria: " + str(memory) + "   CPU: " + str(cpu)) 
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=80, host='0.0.0.0')
